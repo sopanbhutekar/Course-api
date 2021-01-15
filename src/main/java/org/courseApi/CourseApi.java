aee9da8dda9eb865d5ee08d48fb4bcaed30fb28c
sopanbhutekar <Sopan.Bhutekar@transformco.com>
2021-01-15 09:24:35 -0600

    springboot dependancies and basic anotation configuration

---
+++ src/main/java/org/courseApi/CourseApi.java
@@ -0,0 +1,13 @@
+package org.courseApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApi.class,args);
+	}
+
+}
